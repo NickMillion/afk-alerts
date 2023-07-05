7b014fb068a6165e5e1d0884508d65b32e42745b
Nick <37994255+NickMillion@users.noreply.github.com>
2023-07-04 21:09:38 -0400

    So much better

--- afkalerts.py
+++ afkalerts.py
@@ -4,33 +4,19 @@ from python_imagesearch.imagesearch import region_grabber
 import tkinter as tk
 import win32gui
 import time
+from PIL import Image # Only if debugging is on
+import random
 
 notificationIcon = "mellon.ico"
+
+debugging = False
 
 # This is the function that will always be called if an alert is detected. It's kinda janky.
 def alertWindow(hwnd, alert, position=(0, 0)):    
     # Create a new window
     window = tk.Tk()
     window.title(alert)
-    window.geometry("500x100")
-    window.configure(bg="blue")
-    window.iconbitmap(notificationIcon)
-    # Make the window always on top
-    window.attributes("-topmost", True)
-    # 50% opacity
-    window.wm_attributes("-alpha", 0.75)
-    # Tool window
-    window.wm_attributes("-toolwindow", True)
-    window.wm_attributes("-transparentcolor", "blue")
-    # Remove the topbar
     window.overrideredirect(True)
-
-    window.lift()
-
-    # Add a label to the window, size 20 with white text
-    label = tk.Label(window, text=alert, font=("Arial Bold", 20), bg="blue", fg="white")
-    label.pack()
-
     # If position is 0, 0, center the window
     if position == (0, 0):
         # Get the screen's size
@@ -41,23 +27,38 @@ def alertWindow(hwnd, alert, position=(0, 0)):
 
     # Set the window's position
     window.geometry("+%d+%d" % (position[0], position[1]))
+    window.geometry("700x700")
+    window.configure(bg="blue")
+    window.iconbitmap(notificationIcon)
+    # Make the window always on top
+    window.attributes("-topmost", True)
+    # 50% opacity
+    window.wm_attributes("-alpha", 0.75)
+    # Tool window
+    window.wm_attributes("-toolwindow", True)
+    window.wm_attributes("-transparentcolor", "blue")
+    # Remove the topbar
+
+    window.lift()
+
+    alertColors = ["red", "white", "yellow"]
+
+    # Add a label to the window with a randomly selected alert color
+    selectedColor = random.choice(alertColors)
+    label = tk.Label(window, text=alert, font=("Arial Bold", 78), bg="blue", fg=selectedColor)
+    label.pack()
+
 
     # Run the window loop for 5 seconds
-    window.after(5000, window.destroy)
+    window.after(250, window.destroy)
     window.mainloop()
-
-# These are the images we're going to be using, could probably improve this later on
-# Potential improvements: scan the /img/ folder and get all images and use a naming scheme to set them up
-antiMacro = "img/antimacro.PNG"
-lowHP = "img/lowhp.PNG"
-lowPray = "img/lowprayer.PNG"
-trivia = "img/trivia.PNG"
 
 # Print that we're running
 print("AFK Alerts is running...")
 
 # We want the user to input part of the name of the window they want to check
-windowName = input("Enter the name of the window you want to check: ")
+# windowName = input("Enter the name of the window you want to check: ")
+windowName = "Velheim"
 
 # If the user didn't input anything, print that we're exiting and exit
 if windowName == "":
@@ -79,8 +80,8 @@ print("Found " + str(len(windowList)) + " windows that match input.", flush=True
 # Print them all
 print(windowList)
 
-# Scan every 1 second by default, but can be anything
-SCAN_INTERVAL = 1
+# Scan every 1 second by default, but can be anything. The tiny pixel scan alerts take ~0.05 seconds to run
+SCAN_INTERVAL = 0.05
 
 # If panic key is not pressed, loop every scan interval and check if the image is on screen
 while True:
@@ -98,42 +99,51 @@ while True:
         print("Scanning window: " + currentWindow)
 
         # Get window's position and size
-        windowPos = win32gui.GetWindowRect(win32gui.FindWindow(None, currentWindow))
+        window = win32gui.FindWindow(None, currentWindow)
+        windowPos = win32gui.GetWindowRect(window)
+        # Check if the window is focused
+        if win32gui.GetForegroundWindow() == window:
+            # Skip if it's focused
+            continue
 
-        # TODO actually figure out the region to scan
-        # Generate the top right corner region of the window in a 200x200 box
-        # This is where we will be scanning for the red pixels
-        # cornerTuple = (windowPos[2] - 200, windowPos[1], windowPos[2], windowPos[1] + 200)
-        # topRightCorner = region_grabber(cornerTuple)
-        #lowHPPos = imagesearcharea(lowHP, 0.9, cornerTuple[0], cornerTuple[1], cornerTuple[2], cornerTuple[3], topRightCorner)
-        #lowPrayPos = imagesearcharea(lowPray, 0.9, cornerTuple[0], cornerTuple[1], cornerTuple[2], cornerTuple[3], topRightCorner)
+        # windowRegion = region_grabber(windowPos)
+        hp = region_grabber((windowPos[2] - 225, windowPos[1] + 82, windowPos[2] - 195, windowPos[1] + 98))
+        prayer = region_grabber((windowPos[2] - 230, windowPos[1] + 118, windowPos[2] - 203, windowPos[1] + 132))
 
-        # At 0.9 precision the low HP/prayer images are detected pretty consistently
-        # Check if lowHP is on screen
-        lowHPPos = imagesearch(lowHP, precision=0.9)
-        if lowHPPos[0] != -1:
-            alertWindow(win32gui.FindWindow(None, currentWindow), "Low HP")
-            break
-        
-        # Check if lowPray is on screen
-        lowPrayPos = imagesearch(lowPray, precision=0.9)
-        if lowPrayPos[0] != -1:
-            alertWindow(win32gui.FindWindow(None, currentWindow), "Low Prayer")
-            break
-        
-        # Check if antiMacro is on screen
-        antiMacroPos = imagesearch(antiMacro, precision=0.9)
-        if antiMacroPos[0] != -1:
-            alertWindow(win32gui.FindWindow(None, currentWindow), "Answer Anti-Macro")
-            break
+        # Use Pillow to convert the Screenshot of the regions into a file for debugging
+        if debugging:
+            Image.frombytes('RGB', hp.size, hp.bgra, 'raw', 'BGRX').save("debug/hp" + str(i) + ".png")
+            Image.frombytes('RGB', prayer.size, prayer.bgra, 'raw', 'BGRX').save("debug/prayer" + str(i) + ".png")
 
-        continue # Continuing here because the trivia check is needs better handling than this
-        # Check if trivia is on screen
-        triviaPos = imagesearch(trivia, precision=0.9)
-        if triviaPos[0] != -1:
-            alertWindow(win32gui.FindWindow(None, currentWindow), "Answer Trivia")
-            break
-
-    # Print how long it took to scan; this includes the window live time though
-    print("Scanned in " + str(time.time() - timeStarted) + " seconds.", flush=True)
+        # Get a random value between 350 and 400
+        randVal = random.randint(250, 450)
+        alertPosition = (windowPos[0] + 25, windowPos[1] + randVal)
+        alerted = False
+        # Iterate through all the pixels in hp to determine if there are any red
+        for x in range(0, hp.size[0]):
+            for y in range(0, hp.size[1]):
+                # Get the pixel's RGB value
+                pixel = hp.pixel(x, y)
+                # If the pixel is red, alert
+                if pixel[0] > 200 and pixel[1] < 100 and pixel[2] < 100:
+                    alertWindow(win32gui.FindWindow(None, currentWindow), "HP LOW", alertPosition)
+                    alerted = True
+                    break
+            if alerted:
+                break
+        # Same thing for prayer
+        for x in range(0, prayer.size[0]):
+            for y in range(0, prayer.size[1]):
+                pixel = prayer.pixel(x, y)
+                if pixel[0] > 200 and pixel[1] < 100 and pixel[2] < 100:
+                    alertWindow(win32gui.FindWindow(None, currentWindow), "PRAYER LOW", alertPosition)
+                    alerted = True
+                    break
+            if alerted:
+                break
+                
+    if debugging:
+        exit()
+    # Print how long it took to scan; this includes the window live time
+    # print("Scanned in " + str(time.time() - timeStarted) + " seconds.", flush=True)
     time.sleep(SCAN_INTERVAL)
