2ecc82b80ce82302aa4d527c52252654e54c8add
Nick <37994255+NickMillion@users.noreply.github.com>
2023-07-05 22:33:43 -0400

    Why am I still working on this?

--- afkalerts.py
+++ afkalerts.py
@@ -2,20 +2,28 @@ from python_imagesearch.imagesearch import region_grabber
 import tkinter as tk
 import win32gui
 import time
-from PIL import Image # Only if debugging is on
+from PIL import Image # Only used if debugging is on to take screenshot of scanned regions
 import random
 
-notificationIcon = "mellon.ico"
+# How often to scan the windows, in seconds. Should probably be above 0.05 but can otherwise be anything.
+SCAN_INTERVAL = 0.1
+# If debugging is on, it will only do the first scan and print out some results and take screenshots of scanned regions before exiting
+DEBUGGING = False
+# How many seconds before printing the "Running for x minutes." message
+TIME_BETWEEN_PRINTS = 60
+# How long to keep the alert visible, in ms
+ALERT_DURATION = 500
+# Default window name
+DEFAULT_WINDOW_NAME = "718/925"
 
-debugging = False
-
-# This is the function that will always be called if an alert is detected. It's kinda janky.
+# This is the function that will always be called if an alert is detected. It's kinda janky but works for our purposes.
 # Would be fantastic to have it run on a separate thread, based on what I've read? Unsure how to implement that currently.
 def alertWindow(hwnd, alert = "", position=(0, 0), windowSize=(700, 700)):    
     # Create a new window
     window = tk.Tk()
     window.title(alert)
     window.overrideredirect(True)
+
     # If position is 0, 0, center the window
     if position == (0, 0):
         # Get the screen's size
@@ -27,48 +35,55 @@ def alertWindow(hwnd, alert = "", position=(0, 0), windowSize=(700, 700)):
     # Set the window's position
     window.geometry("+%d+%d" % (position[0], position[1]))
     window.geometry(str(windowSize[0]) + "x" + str(windowSize[1]))
-    window.configure(bg="blue")
-    window.iconbitmap(notificationIcon)
     # Make the window always on top
     window.attributes("-topmost", True)
     # 50% opacity
     window.wm_attributes("-alpha", 0.75)
     # Tool window
     window.wm_attributes("-toolwindow", True)
-    window.wm_attributes("-transparentcolor", "blue")
-    # Remove the topbar
 
+    # Black will be transparent
+    window.wm_attributes("-transparentcolor", "black")
+    window.configure(bg="black")
+
+    # Lift the window to the top so it's always visible, since it's an alert, duh
     window.lift()
 
-    alertColors = ["red", "white", "yellow"]
+    # List of colors to randomly select from, should be bright and varied to be attention-grabbing
+    alertColors = ["red", "white", "yellow", "blue"]
 
     # Add a label to the window with a randomly selected alert color
     selectedColor = random.choice(alertColors)
+
     # Text size must be at least 64, divide by the length of the alert to get the size
     # This will make it take up basically the entire window
     textSize = int(windowSize[0] / len(alert))
     if textSize < 64:
         textSize = 64
-    label = tk.Label(window, text=alert, font=("Arial Bold", textSize), bg="blue", fg=selectedColor)
+
+    # Finally, add the label
+    label = tk.Label(window, text=alert, font=("Arial Bold", textSize), bg="black", fg=selectedColor)
     label.pack()
 
 
     # Run the window loop for 500 ms
-    window.after(500, window.destroy)
+    window.after(ALERT_DURATION, window.destroy)
     window.mainloop()
 
+    print("Alerted " + alert + " for window " + str(hwnd), flush=True)
+
 # Print that we're running
-print("AFK Alerts is running...")
+print("AFK Alerts has started!", flush=True)
 
 # We want the user to input part of the name of the window they want to check
-windowName = input("Enter the name of the window you want to check: ")
+windowName = input("Hit enter to use default or enter the name of the window you want to check: ")
 
 # If the user didn't input anything, print that we're exiting and exit
 if windowName == "":
     print("No window name entered, assuming default.")
-    windowName = "718/925"
+    windowName = DEFAULT_WINDOW_NAME
 
-# Make an empty list to store the names of the windows
+# Make an empty list to store the names of the windows and the time since they were last focused
 windowList = []
 timeSinceLastFocused = []
 
@@ -82,58 +97,68 @@ win32gui.EnumWindows(winEnumHandler, None)
 
 # Print all the windows we found that match input
 print("Found " + str(len(windowList)) + " windows that match input.", flush=True)
-# Print them all
-print(windowList)
+# Loop through and print them all separately because the one long line is ugly and hard to read
+for i in range(0, len(windowList)):
+    print("Window " + str(i) + ": " + windowList[i], flush=True)
 
 # If we didn't find any windows, exit
 if len(windowList) == 0:
     print("No windows found, exiting.")
     exit()
 
-# Scan every 1 second by default, but can be anything. The tiny pixel scan alerts take ~0.05 seconds to run
-SCAN_INTERVAL = 0.1
+# Initializing timing variables
 timeElapsed = 0
-# If panic key is not pressed, loop every scan interval and check if the image is on screen
+startTime = time.time()
+minutes = 0
+
+# The actual program loop
 while True:
-    startTime = time.time()
+    # Get how much time has elapsed
+    timeElapsed = time.time() - startTime
+    # If it's been more than 60 seconds, print that we're still running
+    if timeElapsed > TIME_BETWEEN_PRINTS:
+        minutes += 1
+        print("Running for " + str(minutes) + " minutes.", flush=True)
+        startTime = time.time()
+
     # Iterate through all the found windows
     for i in range(0, len(windowList)):
-        # Get the current window's image
+        # Get the current window position and size
         currentWindow = windowList[i]
-
-        # Get window's position and size
         window = win32gui.FindWindow(None, currentWindow)
         windowPos = win32gui.GetWindowRect(window)
         windowSize = (windowPos[2] - windowPos[0], windowPos[3] - windowPos[1])
+
         # Check if the window is focused
         if win32gui.GetForegroundWindow() == window:
-            timeSinceLastFocused[i] = 0
+            timeSinceLastFocused[i] = time.time()
         
-        # If it's not focused, increment the time since it was focused. Pretty sure this is wrong but it doesn't really matter.
-        timeSinceLastFocused[i] += timeElapsed + SCAN_INTERVAL
-        if debugging:
+        if DEBUGGING:
             print("Time since last focused for index " + str(i) + ": " + str(timeSinceLastFocused[i]), flush=True)
 
-        # Put the alertPosition in the center of the window
+        # Put the alertPosition in the center of the window. In some cases it might be a little low, but this is nitpicking
         alertPosition = (windowPos[0], windowPos[1] + (windowSize[1] / 2))
 
-        # If it's been more than ~180 seconds since the window was focused, alert AFK
-        if timeSinceLastFocused[i] > 180:
+        # If it's been more than ~4 mins since the window was focused, alert AFK. This value is in seconds
+        if timeSinceLastFocused[i] - time.time() > 240:
             alertWindow(win32gui.FindWindow(None, currentWindow), "AFK", alertPosition, windowSize)
             continue
 
-
-        # windowRegion = region_grabber(windowPos)
+        # Grab the regions of the screen that we want to check. These are hardcoded.
+        # In the future I could make it detect an image match to automatically select the regions, but that's a lot of work I don't want to bother with.
         hp = region_grabber((windowPos[2] - 225, windowPos[1] + 82, windowPos[2] - 195, windowPos[1] + 98))
         prayer = region_grabber((windowPos[2] - 230, windowPos[1] + 118, windowPos[2] - 203, windowPos[1] + 132))
 
         # Use Pillow to convert the Screenshot of the regions into a file for debugging
-        if debugging:
+        if DEBUGGING:
+            windowRegion = region_grabber(windowPos)
             Image.frombytes('RGB', hp.size, hp.bgra, 'raw', 'BGRX').save("debug/hp" + str(i) + ".png")
             Image.frombytes('RGB', prayer.size, prayer.bgra, 'raw', 'BGRX').save("debug/prayer" + str(i) + ".png")
+            Image.frombytes('RGB', windowRegion.size, windowRegion.bgra, 'raw', 'BGRX').save("debug/windowRegion" + str(i) + ".png")
 
         alerted = False
-        # Iterate through all the pixels in hp to determine if there are any red
+        # Iterate through all the pixels in hp and prayer to determine if there are any red
+        # While the functionality is the same, I'm keeping them separate in case I want to add more alerts later that do something else
         for x in range(0, hp.size[0]):
             for y in range(0, hp.size[1]):
                 # Get the pixel's RGB value
@@ -145,7 +170,6 @@ while True:
                     break
             if alerted:
                 break
-        # Same thing for prayer
         for x in range(0, prayer.size[0]):
             for y in range(0, prayer.size[1]):
                 pixel = prayer.pixel(x, y)
@@ -156,9 +180,7 @@ while True:
             if alerted:
                 break
                 
-    if debugging:
+    if DEBUGGING:
         exit()
-
-    timeElapsed = time.time() - startTime
 
     time.sleep(SCAN_INTERVAL)
